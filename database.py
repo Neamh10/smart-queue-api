7d5d5d935d4cd99134b1466489163e3a8f461d43
Neamh10 <118745922+Neamh10@users.noreply.github.com>
2025-12-16 19:23:09 +0300

    database.py

--- database.py
+++ database.py
@@ -1,9 +1,16 @@
 import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# تحميل متغيرات البيئة
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:raise RuntimeError("DATABASE_URL not set")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL not set")
 
 engine = create_engine(
     DATABASE_URL,
