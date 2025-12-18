1410f57245d57fee75482c110cf917e3e1d45e13
Neamh10 <118745922+Neamh10@users.noreply.github.com>
2025-12-18 09:50:06 +0300

    schemas.py

--- schemas.py
+++ schemas.py
@@ -1,14 +1,16 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
+
 
 class EventIn(BaseModel):
     place_id: str
-    event: str         
-    time: datetime
+    event: str          
+    time: Optional[datetime] = None
+
 
 class EventResponse(BaseModel):
     status: str
     current_count: int
     message: str
-
 
