a22ba33e9e7731a93cbea43d8846a22ea6d87583
Neamh10 <118745922+Neamh10@users.noreply.github.com>
2025-12-18 09:18:43 +0300

    schemas.py

--- schemas.py
+++ schemas.py
@@ -3,12 +3,12 @@ from datetime import datetime
 
 class EventIn(BaseModel):
     place_id: str
-    event: str          # enter | exit
-    event_id: int
+    event: str         
     time: datetime
 
 class EventResponse(BaseModel):
     status: str
     current_count: int
     message: str
+
 
