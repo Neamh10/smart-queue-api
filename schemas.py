beb80278a6b8dee819c296bfc3f52b7070e6ff6d
Neamh10 <118745922+Neamh10@users.noreply.github.com>
2025-12-19 16:36:31 +0300

    schemas.py

--- schemas.py
+++ schemas.py
@@ -2,26 +2,12 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
-
-# ================== INPUT SCHEMA ==================
 class EventIn(BaseModel):
     place_id: str
-    event: str  # "enter" | "exit"
+    event: str          # enter | exit
     time: Optional[datetime] = None
 
-
-# ================== RESPONSE SCHEMA ==================
 class EventResponse(BaseModel):
-    status: str           # OK | FULL
+    status: str
     current_count: int
     message: str
-
-
-# ================== EVENT OUT (HISTORY) ==================
-class EventOut(BaseModel):
-    place_id: str
-    event: str
-    time: datetime
-
-    class Config:
-        from_attributes = True
