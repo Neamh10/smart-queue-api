91359433884e40ed73265685d92f5ec6dcdd4037
Neamh Khalid <118745922+Neamh10@users.noreply.github.com>
2026-01-11 21:59:04 +0300

    database.py

--- database.py
+++ database.py
@@ -1,20 +1,15 @@
 import os
 from dotenv import load_dotenv
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# تحميل متغيرات البيئة
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL not set")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./smart_queue.db")
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True
+    connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(
@@ -25,10 +20,10 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
