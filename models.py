fa24074fa6da20f0ec9df817794fcd668655aec6
Neamh Khalid <118745922+Neamh10@users.noreply.github.com>
2026-01-11 21:57:06 +0300

    models.py

--- models.py
+++ models.py
@@ -1,21 +1,25 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, String, Integer, DateTime, Boolean
+from datetime import datetime, timedelta
 from database import Base
-from datetime import datetime
+
 
 class Place(Base):
     __tablename__ = "places"
 
     place_id = Column(String, primary_key=True, index=True)
-    capacity = Column(Integer, nullable=False)
     current_count = Column(Integer, default=0)
-
-class VisitEvent(Base):
-    __tablename__ = "visit_events"
-
-    id = Column(Integer, primary_key=True, index=True)
-    place_id = Column(String, index=True)
-    event = Column(String)
-    current_count = Column(Integer)
-    time = Column(DateTime, default=datetime.utcnow)
+    capacity = Column(Integer, default=10)
 
 
+class Reservation(Base):
+    __tablename__ = "reservations"
+
+    token = Column(String, primary_key=True, index=True)
+    from_place = Column(String, index=True)
+    to_place = Column(String, index=True)
+    expires_at = Column(DateTime)
+    confirmed = Column(Boolean, default=False)
+
+    @staticmethod
+    def expiry(minutes=3):
+        return datetime.utcnow() + timedelta(minutes=minutes)
