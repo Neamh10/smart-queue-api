0e458bfb0188ce1b609258e988ff4f7619e96fbe
Neamh Khalid <118745922+Neamh10@users.noreply.github.com>
2026-01-17 17:31:52 +0300

    manager.py

--- manager.py
+++ manager.py
@@ -1,23 +1,18 @@
+from typing import Dict, List
 from fastapi import WebSocket
+
 
 class ConnectionManager:
     def __init__(self):
-        # { "hall_1": [ws1, ws2], "hall_2": [ws3] }
-        self.active_connections: dict[str, list[WebSocket]] = {}
+        self.active_connections: Dict[str, List[WebSocket]] = {}
 
     async def connect(self, websocket: WebSocket, place_id: str):
         await websocket.accept()
-        if place_id not in self.active_connections:
-            self.active_connections[place_id] = []
-        self.active_connections[place_id].append(websocket)
+        self.active_connections.setdefault(place_id, []).append(websocket)
 
     def disconnect(self, websocket: WebSocket, place_id: str):
-        if place_id in self.active_connections:
-            self.active_connections[place_id].remove(websocket)
+        self.active_connections[place_id].remove(websocket)
 
     async def broadcast(self, place_id: str, data: dict):
-        if place_id not in self.active_connections:
-            return
-
-        for websocket in self.active_connections[place_id]:
-            await websocket.send_json(data)
+        for ws in self.active_connections.get(place_id, []):
+            await ws.send_json(data)
