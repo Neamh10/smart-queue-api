71b4ece90ba3c9ea988c97c3712dcd2e857e3234
Neamh Khalid <118745922+Neamh10@users.noreply.github.com>
2026-01-17 17:29:06 +0300

    models.py

--- models.py
+++ models.py
@@ -1,30 +1,23 @@
-from sqlalchemy import Column, String, Integer, DateTime, Boolean
-from datetime import datetime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from database import Base
+from datetime import datetime
+
 
 class Place(Base):
     __tablename__ = "places"
 
-    place_id = Column(String, primary_key=True, index=True)
+    id = Column(String, primary_key=True, index=True)
     current_count = Column(Integer, default=0)
     capacity = Column(Integer, default=10)
-
-
-class VisitEvent(Base):
-    __tablename__ = "visit_events"
-
-    id = Column(Integer, primary_key=True, index=True)
-    place_id = Column(String, index=True)
-    event = Column(String)  # enter | exit
-    current_count = Column(Integer)
-    time = Column(DateTime, default=datetime.utcnow)
 
 
 class Reservation(Base):
     __tablename__ = "reservations"
 
-    token = Column(String, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
+    token = Column(String, unique=True, index=True)
     from_place = Column(String, index=True)
     to_place = Column(String, index=True)
     expires_at = Column(DateTime)
     confirmed = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
