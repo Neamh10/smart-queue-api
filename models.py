8286288fc38565820b80bff11a00f6fd182d39dc
Neamh Khalid <118745922+Neamh10@users.noreply.github.com>
2026-01-24 08:27:09 +0300

    models.py

--- models.py
+++ models.py
@@ -1,13 +1,17 @@
 from sqlalchemy import Column, String, Integer, DateTime, Boolean
 from datetime import datetime
 from database import Base
+from enum import Enum
 
-class Place(Base):
+
+cclass Place(Base):
     __tablename__ = "places"
 
-    place_id = Column(String, primary_key=True, index=True)
+    id = Column(String, primary_key=True)
+    name = Column(String)
+    capacity = Column(Integer)
     current_count = Column(Integer, default=0)
-    capacity = Column(Integer, default=10)
+    state = Column(String, default="NORMAL")  
 
 
 class VisitEvent(Base):
@@ -30,7 +34,13 @@ class Reservation(Base):
 
     confirmed = Column(Boolean, default=False)
     archived = Column(Boolean, default=False)
-    expires_at = Column(DateTime)
+    expires_at = Column(DateTime)  
+
+class PlaceState(str, Enum):
+    NORMAL = "NORMAL"
+    FULL = "FULL"
+
+
 
 
 
